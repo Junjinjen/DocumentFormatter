56c19a2d36bf32c029a4bf4acb67c0ef26ab3162
Junjinjen <junjinjen@gmail.com>
2022-08-10 18:50:03 +0300

    Add text wrap formatter

---
+++ DocumentFormatter.Core/Formatters/TextWrapFormatter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace DocumentFormatter.Core.Formatters
+{
+    public class TextWrapFormatter : FormatterBase
+    {
+        protected override string TagName => "br";
+
+        public override void Format(XElement element, StreamWriter writer, Action<XElement, StreamWriter> innerElementsHandler)
+        {
+            innerElementsHandler.Invoke(element, writer);
+            writer.WriteLine();
+        }
+    }
+}
